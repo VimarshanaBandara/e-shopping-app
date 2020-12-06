86a6216c38462b890f0dcac9451e50e3487193b1
VimarshanaBandara <vimarshanabandara76@gmail.com>
2020-12-07 00:47:13 +0530

    Create the Admin Page

--- lib/Admin/adminLogin.dart
+++ lib/Admin/adminLogin.dart
@@ -1,3 +1,5 @@
+
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:e_shop/Admin/uploadItems.dart';
 import 'package:e_shop/Authentication/authenication.dart';
@@ -11,7 +13,22 @@ import 'package:flutter/material.dart';
 class AdminSignInPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [Colors.pink , Colors.lightGreenAccent],
+                  begin: const FractionalOffset(0.0,0.0),
+                  end: const FractionalOffset(1.0,0.0),
+                  stops: [0.0,1.0],
+                  tileMode: TileMode.clamp
+              )
+          ),
+        ),
+      ),
+       body: AdminSignInScreen(),
+    );
   }
 }
 
@@ -23,9 +40,143 @@ class AdminSignInScreen extends StatefulWidget {
 
 class _AdminSignInScreenState extends State<AdminSignInScreen>
 {
+
+  final GlobalKey <FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _adminIdTextEditingController = TextEditingController();
+  final TextEditingController _passwordTextEditingController = TextEditingController();
+
+
   @override
   Widget build(BuildContext context) {
 
-    return SingleChildScrollView();
+    double _screenWidth = MediaQuery.of(context).size.width , _screenHeight = MediaQuery.of(context).size.height;
+    return SingleChildScrollView(
+      child: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [Colors.pink , Colors.lightGreenAccent],
+                begin: const FractionalOffset(0.0,0.0),
+                end: const FractionalOffset(1.0,0.0),
+                stops: [0.0,1.0],
+                tileMode: TileMode.clamp
+            )
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Container(
+              alignment: Alignment.bottomCenter,
+              child: Image.asset('images/admin.png',height: 240.0,width: 240.0,),
+            ),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text("Admin",style: TextStyle(color: Colors.white,fontSize: 28.0,fontWeight: FontWeight.bold),),
+            ),
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+
+                  CustomTextField(
+                    controller:_adminIdTextEditingController ,
+                    data: Icons.person,
+                    hintText: "Id",
+                    isObsecure:false ,
+                  ),
+
+                  CustomTextField(
+                    controller:_passwordTextEditingController ,
+                    data: Icons.person,
+                    hintText: "Password",
+                    isObsecure:true ,
+                  ),
+
+
+                ],
+              ),
+            ),
+
+            RaisedButton(
+              onPressed: () {
+
+                _adminIdTextEditingController.text.isNotEmpty &&
+                    _passwordTextEditingController.text.isNotEmpty
+                    ?loginAdmin()
+                    : showDialog(
+                    context: context,
+                    builder: (c) {
+                      return ErrorAlertDialog(message: "Please input email & password",);
+                    }
+
+                );
+
+              },
+              color: Colors.pink,
+              child: Text("Login",style: TextStyle(color: Colors.white),),
+            ),
+
+            SizedBox(height: 50.0,),
+            Container(
+              height: 4.0,
+              width: _screenHeight * 0.8,
+              color: Colors.pink,
+            ),
+            SizedBox(height: 20.0,),
+
+            FlatButton.icon(
+
+              onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context)=> AuthenticScreen())),
+              icon: (Icon(Icons.nature_people,color: Colors.pink,)),
+              label: Text("I'am Not admin",style: TextStyle(color: Colors.pink,fontWeight: FontWeight.bold),),
+
+            ),
+
+            SizedBox(height: 50.0,),
+
+          ],
+        ),
+      ),
+    );
   }
+
+  loginAdmin(){
+
+   Firestore.instance.collection("admins").getDocuments().then((snapshot){
+   snapshot.documents.forEach((result){
+     if(result.data["id"] != _adminIdTextEditingController.text.trim())
+     {
+      Scaffold.of(context).showSnackBar(SnackBar(content: Text("Your id is not correct")));
+     }
+
+
+
+  else if(result.data["password"] != _passwordTextEditingController.text.trim())
+     {
+       Scaffold.of(context).showSnackBar(SnackBar(content: Text("Your password is not correct")));
+     }
+
+    else
+      {
+        Scaffold.of(context).showSnackBar(SnackBar(content: Text("Welcome dear admin"+result.data["name"])));
+
+        setState(() {
+          _adminIdTextEditingController.text ="";
+          _passwordTextEditingController.text ="";
+        });
+
+        Route route = MaterialPageRoute(builder: (c) => UploadPage());
+        Navigator.pushReplacement(context, route);
+
+
+     }
+
+   });
+   });
+
+
+  }
+
+
+
+
 }
--- lib/Admin/uploadItems.dart
+++ lib/Admin/uploadItems.dart
@@ -19,9 +19,134 @@ class UploadPage extends StatefulWidget
 class _UploadPageState extends State<UploadPage> with AutomaticKeepAliveClientMixin<UploadPage>
 {
   bool get wantKeepAlive => true;
+  File file;
 
   @override
   Widget build(BuildContext context) {
-    return Text("");
+    return displayAdminHomeScreen();
   }
+
+  displayAdminHomeScreen(){
+    return Scaffold(
+      appBar: AppBar(
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [Colors.pink , Colors.lightGreenAccent],
+                  begin: const FractionalOffset(0.0,0.0),
+                  end: const FractionalOffset(1.0,0.0),
+                  stops: [0.0,1.0],
+                  tileMode: TileMode.clamp
+              )
+          ),
+        ),
+         leading: IconButton(
+           icon: Icon(Icons.border_color,color: Colors.white,),
+           onPressed: () {
+             Route route = MaterialPageRoute(builder: (c) => AdminShiftOrders());
+             Navigator.pushReplacement(context, route);
+             },
+         ),
+        actions: [
+          FlatButton(
+            child: Text("Logout",style: TextStyle(color: Colors.pink,fontWeight: FontWeight.bold,fontSize: 16.0),),
+            onPressed: ()
+            {
+              Route route = MaterialPageRoute(builder: (c) => SplashScreen());
+              Navigator.pushReplacement(context, route);
+            },
+          )
+        ],
+
+      ),
+
+      body: getAdminHomeScreenBody(),
+    );
+  }
+
+  getAdminHomeScreenBody()
+  {
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              colors: [Colors.pink , Colors.lightGreenAccent],
+              begin: const FractionalOffset(0.0,0.0),
+              end: const FractionalOffset(1.0,0.0),
+              stops: [0.0,1.0],
+              tileMode: TileMode.clamp
+          )
+      ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.shop_two,color: Colors.white,size: 200.0,),
+            Padding(
+              padding: EdgeInsets.only(top: 20.0),
+              child: RaisedButton(
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(9.0)),
+                child: Text('Add New Items',style: TextStyle(color: Colors.white,fontSize: 20.0),),
+                color: Colors.green,
+                onPressed: () => takeImage(context),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  takeImage(mContext)
+  {
+    return showDialog(
+        context: mContext,
+       builder: (con)
+       {
+          return SimpleDialog(
+            title: Text("Item Image",style: TextStyle(color: Colors.green,fontWeight: FontWeight.bold),),
+            children: [
+            SimpleDialogOption(
+              child: Text('Capture with Camera',style: TextStyle(color: Colors.green,),),
+              onPressed: capturePhotoWithCamera,
+            ),
+
+              SimpleDialogOption(
+                child: Text('Select from Gallery',style: TextStyle(color: Colors.green,),),
+                onPressed: pickPhotoFromGallery,
+              ),
+              SimpleDialogOption(
+                child: Text('Cancel',style: TextStyle(color: Colors.green,),),
+                onPressed: ()
+                {
+                  Navigator.pop(context);
+                },
+              )
+
+            ],
+          );
+
+
+       }
+    );
+  }
+
+  capturePhotoWithCamera()async
+  {
+    Navigator.pop(context);
+    File imageFile =  await ImagePicker.pickImage(source:ImageSource.camera , maxHeight: 680.0 , maxWidth: 970.0);
+    setState(() {
+      file = imageFile;
+    });
+
+  }
+
+  pickPhotoFromGallery()async
+  {
+    Navigator.pop(context);
+    File imageFile =  await ImagePicker.pickImage(source:ImageSource.gallery );
+    setState(() {
+      file = imageFile;
+    });
+  }
+
 }
